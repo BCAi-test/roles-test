0cd4139c2dd5384dae8a794b8fa5eefc103cef50
BCAi - NoReply <bcains.noreply@gmail.com>
2024-02-05 17:37:53 +0700

    first commit

---
+++ halo.js
@@ -0,0 +1,10 @@
+//baseline folder
+//tambahan komen
+
+//tambah lagi
+const firstName = 'Faishal'
+const lastName = 'Adiyatma'
+
+const completeName = firstName +' '+ lastName
+
+console.log(completeName)
